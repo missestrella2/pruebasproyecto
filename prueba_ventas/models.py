db516ed249657d3e6f4bd06a92bc41991c156fd0
missestrella2 <missestrella2@gmail.com>
2022-11-29 01:18:40 -0300

    correcion login nuevo y relacion entre tablas

---
+++ prueba_ventas/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+#from ast import mod
+#from email.policy import default
+#from tabnanny import verbose
+from django.db import models
+from pagoformas.models import PagoForma
+
+
+class Venta(models.Model):
+    fecha_de_venta=models.DateField(verbose_name='Fecha de Venta')
+    monto= models.CharField(max_length=100, default=" ", verbose_name='Monto')
+    pagoformas = models.ManyToManyField(PagoForma)
